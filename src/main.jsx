aff4f68c6c3def76bc2439bc82fce0d1dcd6c33c
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-10-11 23:21:08 +1100

    added browser routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    // children: [
+    //   {
+    //     path: "/",
+    //     element: <Home />,
+    //   },
+    //   {
+    //     path: "/projects",
+    //     element: <Projects />,
+    //   },
+    //   {
+    //     path: "/contact",
+    //     element: <Contact />,
+    //   },
+    // ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
