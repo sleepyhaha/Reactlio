bb40af63212126b1dbfb98862d5bbc63511ae3ca
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-10-12 16:57:32 +1100

    added skeleton code for home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="w-full">
+      <h1 className="text-5xl">About me:</h1>
+      <p>I am trying my best to be a software eng pls hire me</p>
+    </div>
+  );
+}
