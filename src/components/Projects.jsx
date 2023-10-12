6d4b6ce369dc05669bbf870b24afd3047014cc93
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-10-12 17:17:25 +1100

    added projects page

---
+++ src/components/Projects.jsx
@@ -0,0 +1,13 @@
+export default function Projects() {
+  return (
+    <div>
+      <h1>My Projects</h1>
+      <section>
+        <div>Project 1</div>
+        <div>Project 1</div>
+        <div>Project 1</div>
+        <div>Project 1</div>
+      </section>
+    </div>
+  );
+}
