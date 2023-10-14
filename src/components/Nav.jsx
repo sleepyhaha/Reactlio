c75ebabe887d93bf9c529eb01e9a02bfb8ecf571
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-10-14 15:32:13 +1100

    fixed nav button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,28 +1,23 @@
-import { Link, useLocation } from "react-router-dom";
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
-  const currentPage = useLocation().pathname;
-
   return (
     <nav className="w-screen h-16 bg-slate-700 text-slate-300 text-center flex sticky z-100 px-4">
-      <button className="rounded-md  p-7 active:ring hover:bg-slate-600 active:ring-slate-200">
-        <Link to="/" className="relative bottom-2 font-bold">
+      <Link to="/" className="relative font-bold">
+        <button className="rounded-md p-5 hover:bg-slate-500 active:ring-slate-200">
           Home
-        </Link>
-      </button>
-      <button className="rounded-md  p-7 active:ring hover:bg-slate-600 active:ring-slate-200">
-        <Link to="/projects" className="relative bottom-2 font-bold">
+        </button>
+      </Link>
+      <Link to="/projects" className="relative  font-bold">
+        <button className="rounded-md  p-5 active:ring hover:bg-slate-500 active:ring-slate-200">
           Projects
-        </Link>
-      </button>
-      <button className="rounded-md p-7 active:ring hover:bg-slate-600 active:ring-slate-200">
-        <Link to="/contact" className="relative bottom-2 font-bold">
+        </button>
+      </Link>
+      <Link to="/contact" className="relative  font-bold">
+        <button className="rounded-md p-5 active:ring hover:bg-slate-500 active:ring-slate-200">
           Contact
-        </Link>
-      </button>
+        </button>
+      </Link>
     </nav>
   );
 }
